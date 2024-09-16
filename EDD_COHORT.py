5195a0748080b30f08d9dc71016544db8b32e3dd
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-09-16 06:34:47 +0300

    Update EDD_COHORT.py

--- EDD_COHORT.py
+++ EDD_COHORT.py
@@ -6,5 +6,6 @@ import datetime as dt
 from IPython.display import display
 import streamlit as st
 
-st.markdown("<h4><b>PMTCT CEREBELLUM</b></h4>", unsafe_allow_html=True)
+cola, colb = st.columns([1,3])
+colb.markdown("<h4><b>PMTCT CEREBELLUM</b></h4>", unsafe_allow_html=True)
 st.markdown("<p><b><i>Know where your mothers are at any time t</i></b></p>", unsafe_allow_html=True)
