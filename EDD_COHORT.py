a92568bda9e53b23ad6b9ec8347089d10659098c
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-09-17 07:29:31 +0300

    Update EDD_COHORT.py

--- EDD_COHORT.py
+++ EDD_COHORT.py
@@ -65,6 +65,7 @@ try:
 except:
      st.write("POOR NETWORK, COULDN'T CONNECT TO ANC DATABASE")
      st.stop()
+st.write(pm['EDD'])
 if 'pm_df' not in st.session_state:
      st.session_state.pm_df = pm
      pm = st.session_state.pm_df
